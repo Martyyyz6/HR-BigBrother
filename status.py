66bd995e035c6d233d1e2cef8418a7ec081c43e8
Martyyyz6 <martinkoenders@outlook.com>
2025-01-19 13:30:14 -0300

    Update status.py

--- status.py
+++ status.py
@@ -8,8 +8,7 @@ query = str
 
 
 session.headers["User-Agent"] = "Mozilla/5.0 (Macintosh; Intel Mac OS X 12.2; rv:91.0) Gecko/20100101 Firefox/91.0"
-session.headers["Authorization"] = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsInVzZXJfaWQiOjI0NDY0MjJ9.eyJpc3MiOiJldmVyc2tpZXMuY29tIiwiYXVkIjoiYXV0aCIsImp0aSI6IlFaWGpQSkVtLUFPa3BtajFYaG5xZEE0Wk1ZbGtGXzhvIiwiaWF0IjoxNjQ3MTgxNTUzLjAxMjk5NCwibmJmIjoxNjQ3MTgxMjUzLjAxMjk5NywiZXhwIjoxNjQ3MjY3OTUzfQ.2ZSoYhCXTkh3-R8O2DeLIf-Vb0fZWQC99OYt8T-2J0k"
-
+session.headers["Authorization"] = "Bearer
 
 def func1(session:Session, query:str) -> dict:
   results = session.get(f"https://api.everskies.com/search?query={query}").json()
